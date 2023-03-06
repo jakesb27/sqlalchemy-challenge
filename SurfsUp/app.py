6fac1e9f16dade262358fc9d68c96755f29f70b9
Jakesb27 <jake.boden27@gmail.com>
2023-03-05 21:45:32 -0800

    added comments

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -9,11 +9,13 @@ from sqlalchemy import create_engine, func
 # Database Setup
 #################################################
 
+# Create engine connection
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+
+# Reflect the tables
 Base.prepare(autoload_with=engine)
 
 # Save references to each table
@@ -41,42 +43,46 @@ def index():
         "<a href='/api/v1.0/precipitation'> /api/v1.0/precipitation <a/><br/>"\
         "<a href='/api/v1.0/stations'> /api/v1.0/stations <a/><br/>"\
         "<a href='/api/v1.0/tobs'> /api/v1.0/tobs <a/><br/>"\
-        "/api/v1.0/{start}<br/>"\
-        "/api/v1.0/{start}/{end}"
+        "/api/v1.0/{start date}<br/>"\
+        "/api/v1.0/{start date}/{end date}"
 
     return routes
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    """Route for handling precipitation api"""
 
     return "Precipitation"
 
 
 @app.route("/api/v1.0/stations")
 def stations():
+    """Route for handling stations api"""
     
     return "stations"
 
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    """Route for handling temperature api"""
     
     return "tobs"
 
 
 @app.route("/api/v1.0/<start>")
 def start_date(start):
+    """Route for handling min, max, and avg for dates greater or equal to start"""
     
     return f"Start date: {start}"
 
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end_date(start, end):
+    """Route for handling min, max, and avg for dates between start and end"""
     
     return f"Start date: {start}<br/>"\
             f"End date: {end}"
-
 
 
 if __name__ == "__main__":
